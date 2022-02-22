2553a74c97dc4cbbe00c00edfbd24bf1a8c9e038
finaritra <finaritratojoniriana@gmail.com>
2022-02-22 09:55:36 +0300

    Mahay component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Head from './head';
+import Footer from './footer';
 
 function App() {
   return (
@@ -7,8 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload hhjh.
         </p>
+        <Head></Head> Apres head
+        <Footer></Footer>
         <a
           className="App-link"
           href="https://reactjs.org"
